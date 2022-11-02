f08c7875c533317d8e61a74d8e700c3b7d829215
Leandro Echezuri <leandro.echezuri@gmail.com>
2022-11-02 18:44:54 -0300

    rutas-productos-terminadas

--- back/routes/Usuarios.js
+++ back/routes/Usuarios.js
@@ -8,6 +8,12 @@ router.get("/", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+router.get("/me", (req, res) => {
+  Usuarios.findOne({ where: { /*logueado == true*/ } })
+    .then((result) => res.status(200).send(result))
+    .catch((err) => console.log(err));
+});
+
 router.get("/:id", (req, res) => {
   const id = req.params.id;
   Usuarios.findOne({ where: { id } })
@@ -20,10 +26,38 @@ router.post("/registro", (req, res) => {
     .then((result) => res.status(201).send(result))
     .catch((err) => console.log(err));
 });
-router.put("/:id", (req, res) => {
-  //   const { rol, direccion, favoritos } = req.body;
-  const id = req.params.id;
 
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  Usuarios.findOne({ where: { email } }).then((user) => {
+    if (!user) return res.sendStatus(401);
+    // user.validarPassword(password).then((isValid) => {
+    //   if (!isValid) return res.sendStatus(401);
+    console.log("login");
+    res.status(200).send(true);
+
+    // const payload = {
+    //   email: user.email,
+    //   favs: user.favorites,
+    // };
+
+    // const token = generateToken(payload);
+
+    // res.cookie("token", token);
+
+    // res.send(payload);
+    // });
+  });
+});
+router.post("/logout", (req, res) => {
+  // Borrar el token
+  //Setear lo que sea a "deslogueado"
+  //Setear usuario actual a "null"
+  console.log("logout");
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
   Usuarios.update(req.body, { where: { id } }).then((result) =>
     res.status(202).send(result)
   );
--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
 const usersRouter = require("./Usuarios");
+const productosRouter = require("./Productos");
+const carritosRouter = require("./Carritos");
 
 const router = express.Router();
 
 router.use("/usuario", usersRouter);
+router.use("/carritos", carritosRouter);
+router.use("/productos", productosRouter);
 
 module.exports = router;
