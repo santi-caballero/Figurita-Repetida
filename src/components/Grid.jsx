f137dffe6ca0626109a43c83801b1fa5d3a848d3
Bautistagl <bautistagonzalezlazo@gmail.com>
2022-11-04 13:26:21 -0300

    Grid terminada

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,50 +1,24 @@
 import React from "react";
-import Cardd from "../commons/Card";
+import Card from "../commons/Card";
 import fotoMessi from "../commons/utils/messi.jpeg";
 import "./componentClass.css";
 import Link from "react";
+import { Container, Grid } from "@mui/material";
 
-const Grid = ({productos}) => {
+const Grilla = ({ productos }) => {
   // if(!productos) return <p>Esa figurita no existe</p>
 
-  const producto = {
-    type: "jugador",
-    nombre: "Lionel Andres",
-    apellido: "Messi",
-    posicion: "Delantero",
-    pais: "Argentina",
-    precio: "que juegue messi no tiene precio",
-    stock: 1,
-    id: 4,
-  };
-
   return (
-    <section className="gridContainer">
-      {/* CUANDO TENGAMOS LA INFO REAL*/}
-      {/* {productos.map((producto) => (
-        <Cardd
-          ={()=>{handleProducto()}}
-          //margenes tamaños
-          key={producto.id}
-
-          HAY QUE PASARLE A CARD LA IMAGEN
-          producto={producto}
-          fotoMessi={fotoMessi}
-          producto={produto}
-        />
-      ))} */}
-      {/* //PRUEBA ELEMENTO POR ELEMENTO */}
-
-      <Cardd fotoMessi={fotoMessi} />
-      <Cardd fotoMessi={fotoMessi} />
-      <Cardd fotoMessi={fotoMessi} />
-      <Cardd fotoMessi={fotoMessi} />
-      <Cardd fotoMessi={fotoMessi} />
-      <Cardd fotoMessi={fotoMessi} />
-      <Cardd fotoMessi={fotoMessi} />
-      <Cardd fotoMessi={fotoMessi} />
-    </section>
+    <Container maxWidth="md">
+      <Grid container spacing={4}>
+        {productos.map((producto, i) => (
+          <Grid item key={i} xs={12} sm={6} md={4}>
+            <Card product={producto} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
-export default Grid;
+export default Grilla;
