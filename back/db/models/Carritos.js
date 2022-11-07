40ec9c99df6df0ccc4a4bbbfdca25c7b4c568e80
Geronimoz17 <geronimo.zamora16@gmail.com>
2022-11-07 18:23:09 -0300

    routeo de carrito

--- back/db/models/Carritos.js
+++ back/db/models/Carritos.js
@@ -1,10 +1,24 @@
 const db = require("../index");
 const S = require("sequelize");
+const Pedidos = require("./Pedidos");
+const Productos = require("./Productos");
 
 class Carritos extends S.Model {
   comprar(carrito) {
     carrito.update({ comprado: true });
     return Carritos.create({ usuarioId: carrito.usuarioId }).then((res) => res);
+  }
+  calcularPrecioTotal(carrito) {
+    Pedidos.findAll({
+      where: { carritoId: carrito.id },
+      include: Productos,
+    }).then((pedidos) => {
+      console.log("ENTRE A PEDIDOS");
+      carrito.preciototal = pedidos.reduce((suma, pedido) => {
+        return suma + pedido.producto.precio;
+      }, 0);
+    });
+    return carrito;
   }
 }
 
